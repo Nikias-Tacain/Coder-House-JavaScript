673d515ba9c4139a8610f89326dc465000a20314
Nikias Tacain <nikiastacain@yahoo.com.ar>
2022-08-23 14:54:09 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// let food = prompt("Que vas a pedir ?");
+
+
+// switch (food) {
+//     case "hamburguesa":
+//         console.log(`Pedido: ${food}, valor $800`);
+//         break;
+//     case "empanadas":
+//         console.log(`Pedido: ${food}, valor $75 cada una`);
+//         break;
+//     case "lomo":
+//         console.log(`Pedido: ${food}, valor $1000`);
+//         break;
+//     default:
+//         console.log("no elegiste nada");
+//         break;
+// }
+
+// let option = prompt(`Elija una opcion:
+// 1: Garra
+// 2: Batman
+// 3: SpiderHead
+// 4: Terraria
+// `)
+
+// switch (option) {
+//     case "1":
+//         console.log("Usted a elegido la pelicula Garra.");
+//         console.log("Se estreno en el año 2022 y el deporte es el basquetball");
+//         break;
+//     case "2":
+//         console.log("Usted a elegido la pelicula Batman.");
+//         console.log("Se estreno en el año 2022 y se trata de un super heroe");
+//         break;
+//     case "3":
+//         console.log("Usted a elegido la pelicula SpiderHead.");
+//         break;
+//     case "4":
+//         console.log("Usted a elegido la pelicula Terraria.");
+//     default:
+//         console.log("Usted no a elegido ninguna pelicula de la lista. Porfavor si desea ver alguna, elijala.");
+//         break;
+// }
