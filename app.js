6acd7a71076b4760a60e4072107c3206bfd20559
Nikias Tacain <nikiastacain@yahoo.com.ar>
2022-09-08 22:17:19 -0300

    "primera entrega final"

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-function productos(nombre, precio, stock) {
-    this.nombre = nombre
-    this.precio = precio
-    this.stock = stock 
-}
+let ingresoNombre = prompt("Has Ingresado a Green Love, ¿Cual es su nombre?")
 
-const Injertos = new productos("Injertos", 500, "5 unidades")
-const VelasSoja = new productos("Velas de soja", 750, "5 unidades")
-const PortamacetaZinc = new productos("Portamaceta Zinc", 900, "5 unidades")
-const PortamacetaTrioVintage = new productos("Portamaceta Trio Vintage", 1200, "5 unidades")
-const EsferasFloresPreservadas = new productos("Esferas Flores Preservadas", 1500, "5 unidades")
+alert("Bienvenido/a" + " " + ingresoNombre.toUpperCase() + " " + "estos son nuestros productos:")
 
-let nombre = prompt("Buenos dias, ¿cual es su nombre?")
-let correo = prompt("Introduzca su correo electronico para futuras compras. Gracias")
-alert("¡Que tenga un buen dia!" + " " + nombre.toUpperCase())
-do {
-    let catalogo = prompt(`Has ingresado al catalogo de Green Love. (Escriba la opcion que desea elegir)
-
+    let catalogo = prompt(`(Escriba la opcion que desea elegir)
             Injertos
             Velas de soja
             Portamaceta zinc
@@ -79,11 +66,115 @@ do {
             const carrito = []
             carrito.push(catalogo.toUpperCase())
             alert("Carrito de compras"+ "\n" + carrito)
-} while (nombre);
 
-/*const dom = document.querySelectorAll("h2")
-const dom = document.querySelectorAll(".hola")
-const dom = document.querySelectorAll("#hola")
+/*Productos tienda*/
 
 
-console.log(dom);*/
+const productos = document.querySelector(".tienda")
+
+function creartarjeta([img,producto,nombre,precio]) {
+    let code = `
+    <div class="tarjetas">
+        <img src="${img}" alt="${producto}">
+        <div class="tarjetas__texto">
+            <h2 class="tarjetas__nombre">${nombre}</h2>
+            <p class="tarjetas__precio">${precio}</p>
+        </div>
+    </div>
+    `
+    productos.innerHTML += code;
+}
+
+let injertos = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/07/greenlove/entities/img-20191018-wa0040-28201346.jpg",
+"injertos",
+"Injertos",
+"$ 1300"]
+
+let velasSoja = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/img-20210712-183007-1442-16225856.jpg",
+"Velas de soja",
+"Velas de soja",
+"$ 600"]
+
+let comboDuo = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/-20220623-192544-00002-16230554.png",
+"Combos dúos velas + sucus",
+"Combos dúos velas + sucus",
+"$ 1200"]
+
+let portamacetaZinc = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-1411002-12180212.jpg",
+"Portamaceta zinc",
+"Portamaceta zinc",
+"$ 990"]
+
+let macetasCemento = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20210111-010433-16193916.jpg",
+"Macetas de cemento",
+"Macetas de cemento",
+"$ 700"]
+
+let portamacetaTrioVintage = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-141007-16221043.jpg",
+"Portamaceta trio vintage",
+"Portamaceta trio vintage",
+"$ 2300"]
+
+let macetasPlasticasM10 = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-1341382-17001027.jpg",
+"Macetas plásticas estampadas M10",
+"Macetas plásticas estampadas M10",
+"$ 500"]
+
+let macetasPlasticasM6 = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-141317-17001458.jpg",
+"Macetas plásticas estampadas M6",
+"Macetas plásticas estampadas M6",
+"$ 150"]
+
+let portamacetaZincchico = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-140634-17002204.jpg",
+"Portamacetas zinc",
+"Portamacetas zinc",
+"$ 600"]
+
+let macetaZinc = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220619-141214-17002452.jpg",
+"Maceta Zinc",
+"Maceta Zinc",
+"$ 650"]
+
+let ramoFloresPreservadas = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20210712-160011-03135131.jpg",
+"Ramos flores preservadas",
+"Ramos flores preservadas",
+"$ 1200"]
+
+let bouquetsPartys = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20210711-203025-08215455.jpg",
+"Bouquets partys",
+"Bouquets partys",
+"$ 250"]
+
+let ramosGrandes = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20210711-202726-08215929.jpg",
+"Ramos grandes",
+"Ramos grandes",
+"$ 1500"]
+
+let EsferasFloresPreservadas = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/img-20220808-wa0149-12161432.jpg",
+"Esferas con flores preservadas",
+"Esferas con flores preservadas",
+"$ 600"]
+
+let coronasRusticas = ["https://www.greenlove.ar/images/cache/tpt700x700/2022/08/greenlove/entities/20220709-1316232-16223800.jpg",
+"Coronas rústicas",
+"Coronas rústicas",
+"$ 1500"]
+
+
+
+
+creartarjeta(injertos)
+creartarjeta(velasSoja)
+creartarjeta(comboDuo)
+creartarjeta(portamacetaZinc)
+creartarjeta(macetasCemento)
+creartarjeta(portamacetaTrioVintage)
+creartarjeta(macetasPlasticasM10)
+creartarjeta(macetasPlasticasM6)
+creartarjeta(portamacetaZincchico)
+creartarjeta(macetaZinc)
+creartarjeta(ramoFloresPreservadas)
+creartarjeta(bouquetsPartys)
+creartarjeta(ramosGrandes)
+creartarjeta(EsferasFloresPreservadas)
+creartarjeta(coronasRusticas)
