90e767cb3fff6ddeed3aa8af026954c37cc054ae
Nikias Tacain <nikiastacain@yahoo.com.ar>
2022-09-28 05:03:08 -0300

    Arreglando cuestionario

--- app.js
+++ app.js
@@ -5,42 +5,30 @@ function armandoCarrito() {
     const carritoCompleto = document.getElementById("carro")
     let carritoVacio = document.createElement("span")
     let carritoNoVacio = document.createElement("span")
-    carritoVacio.innerHTML = (carrito.length === 0 && `Carrito vacio.Porfavor dirijase a `)
+    carritoVacio.innerHTML = (carrito.length === 0 && `El carrito se encuentra vacio...`)
     carritoCompleto.appendChild(carritoVacio)
     carritoNoVacio.innerHTML = (carrito.length !== 0 && carrito)
 }
 armandoCarrito()
-/*alerta sweetAlert*/
-let botonSweetAlert = document.querySelector('buttonProductos')
+/*alsweetAlert*/
+const botonSweetAlert = document.querySelector('buttonProductos')
 botonSweetAlert.addEventListener('click', () => {
     Swal.fire(
-        'The Internet?',
-        'That thing is still around?',
-        'question'
+        'Good job!',
+        'You clicked the button!',
+        'success'
     )
 });
-/*manipulacion del input celular*/
-const alertaNombre = document.querySelectorAll(".numero")
-alertaNombre.forEach(alertaNombreButton => {
-    alertaNombreButton.addEventListener(`input`, imprimirTextoNombre)
-})
-function imprimirTextoNombre() {
-    const divdatosNombre = document.getElementById("botonDatoNombre")
-    let parrafoNombre = document.createElement("span")
-    parrafoNombre.innerHTML = ("✔Telefono Celular ingresado correctamente.")
-    divdatosNombre.appendChild(parrafoNombre)
-}
-/*manipulacion del input enviar*/
-const alertaEnviar = document.querySelectorAll(".buttonEnviar")
-alertaEnviar.forEach(alertaEnviarButton => {
-    alertaEnviarButton.addEventListener("click", imprimirTextoEnviar)
-})
-function imprimirTextoEnviar() {
-    const divdatosEnviar = document.getElementById("botonDatoEnviar")
-    let parrafoEnviar = document.createElement("span")
-    parrafoEnviar.innerHTML = (`✔Gracias por su compra, el resumen de la misma sera enviado a su correo electronico.`)
-    divdatosEnviar.appendChild(parrafoEnviar)
+/*agregar elementos*/
+function mostrarMensajeAdress (){
+    let inputvalueName = document.getElementById("name").value;
+    let inputvalue = document.getElementById("adress").value;
+    let mensajeInputs = document.getElementById("mensaje")
+    mensajeInputs.innerHTML =`Gracias ${inputvalueName} !!!, los datos fueron registrados correctamente, te mandaremos el resumen de su compra, a esta direccion de correo electronico: ${inputvalue}`
+    let formulario = document.getElementById("customer")
+    formulario.innerHTML = ""
 }
 
+/*Productos --- Tienda*/
+
 /*localstorage*/
-localStorage.setItem(carrito, 1)
