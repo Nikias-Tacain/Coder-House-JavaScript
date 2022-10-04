2bb1e3d3bae374ac5cc4c8e37ef3174c188d01a3
Nikias Tacain <nikiastacain@yahoo.com.ar>
2022-10-03 23:26:13 -0300

    api

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-/*sin articulos en el carrito*/
-function mostrarMensajeAdress (){
-    let inputvalueName = document.getElementById("name").value;
-    let inputvalue = document.getElementById("adress").value;
-    let mensajeInputs = document.getElementById("mensaje")
-    mensajeInputs.innerHTML =`Gracias ${inputvalueName} !!!, los datos fueron registrados correctamente, te mandaremos el resumen de su compra, a esta direccion de correo electronico: ${inputvalue}`
-    let formulario = document.getElementById("customer")
-    formulario.innerHTML = ""
-}
-/*Productos --- Tienda*/
-const stockProductos = [
-    {id: 1, nombre: "Injertos",precio: 1600,imagen: "../logos/injertos.svg"},
-    {id: 2, nombre: "Velas de soja",precio: 600,imagen: "../logos/Velas-de-soja.svg"},
-    {id: 3, nombre: "Combo Duo Velas + Sucus",precio: 1200,imagen: "../logos/Combo-duovelas-sucus.svg"},
-    {id: 4, nombre: "Portamaceta Zinc",precio: 990,imagen: "../logos/PortamacetaZinc.svg"},
-    {id: 5, nombre: "Portamaceta Trio Vintage",precio: 2300,imagen: "../logos/Portamaceta-Trio-Vintage.svg"},
-    {id: 6, nombre: "Ramos Flores Preservadas",precio: 1200,imagen: "../logos/Ramos-Flores-Preservadas.svg"},
-    {id: 7, nombre: "Bouquets Partys",precio: 250,imagen: "../logos/BouquetsPartys.svg"},
-    {id: 8, nombre: "Coronas Rusticas",precio: 1500,imagen: "../logos/Coronas-Rusticas.svg"},
-    {id: 9, nombre: "Esferas con flores preservadas",precio: 600,imagen: "../logos/Esferas-flores-preservadas.svg"}
-]
-/*agregar elementos*/
-
-let contenedorProductos = document.getElementById('divTarjetas');
-
 const contenedorCarrito = document.getElementById('carritoContenedor')
 
 const vaciarCarrito = document.getElementById('vaciar-carrito')
 
 const precioTotal = document.getElementById('precioTotal')
 
+const formularioCarrito = document.getElementById('finalizarCompra')
+
+const formularioDiv = document.getElementById('datosDiv')
+/*Productos --- Tienda*/
+let contenedorProductos = document.getElementById('divTarjetas');
+fetch("./data.json")
+.then((res)=>res.json())
+.then((data)=>{
+    data.forEach((producto) => {
+        const div = document.createElement("div");
+        div.classList.add('tarjetas__texto', 'tarjetas');
+        div.innerHTML = `
+        <img class="tarjetas__img" src= ${producto.imagen} alt="...">
+        <h3 class="tarjetas__nombre">${producto.nombre}</h3>
+        <p class="tarjetas__precio">Precio:$ ${producto.precio}</p>
+        <button id="agregar${producto.id}" class="boton-agregar tarjetas__botton">Agregar al carrito</button>`
+        contenedorProductos.appendChild(div);
+    
+        const botonPress = document.getElementById(`agregar${producto.id}`);
+        botonPress.addEventListener('click', () => {
+            agregarAlCarrito(producto.id);
+        });
+    });
+})
 /*carrito de compras*/
 let carrito = []
 
@@ -40,32 +39,28 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 vaciarCarrito.addEventListener('click', () => {
-    carrito.length = 0
     Swal.fire({
-        title: 'Carrito Vaciado!',
-        text: 'Has eliminado el carrito correctamente !',
-        icon: 'success',
-        confirmButtonText: 'Ok'
+        title: '¿Deseas borrar todo tu carrito?',
+        showDenyButton: true,
+        confirmButtonText: 'Borrar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire('Se ha eliminado correctamente el carrito', '', 'success')
+            carrito.length = 0
+            actualizarCarrito()
+        } else if (result.isDenied) {
+          Swal.fire('Mantuviste tu carrito a salvo', '', 'info')
+        }
       })
-    actualizarCarrito()
 })
-
-stockProductos.forEach((producto) => {
-    const div = document.createElement("div");
-    div.classList.add('tarjetas__texto', 'tarjetas');
-    div.innerHTML = `
-    <img class="tarjetas__img" src= ${producto.imagen} alt="...">
-    <h3 class="tarjetas__nombre">${producto.nombre}</h3>
-    <p class="tarjetas__precio">Precio:$ ${producto.precio}</p>
-    <button id="agregar${producto.id}" class="boton-agregar tarjetas__botton">Agregar al carrito</button>`
-    contenedorProductos.appendChild(div);
-
-    const botonPress = document.getElementById(`agregar${producto.id}`);
-    botonPress.addEventListener('click', () => {
-        agregarAlCarrito(producto.id);
-    });
-});
 const agregarAlCarrito = (prodId) => {
+    Toastify({
+
+        text: "Se ha agregado correctamente al carrito. (Desliza hacia abajo para ver lo que agregaste)",
+        
+        duration: 2500
+        
+    }).showToast();
     const existe = carrito.some (prod => prod.id === prodId)
 
     if (existe) {
@@ -83,6 +78,13 @@ actualizarCarrito()
 };
 
 const eliminarDelCarrito = (prodId) => {
+    Toastify({
+
+        text: "Ha sido eliminado con exito.",
+        
+        duration: 1500
+        
+    }).showToast();
     const item = carrito.find((prod) => prod.id === prodId)
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1)
@@ -102,11 +104,49 @@ const actualizarCarrito = () => {
 
         contenedorCarrito.appendChild(div)
 
-        localStorage.setItem('carrito', JSON.stringify(carrito))
-
     })
-    precioTotal.innerHTML = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+    precioTotal.innerHTML = carrito.reduce((acc , prod)=> acc + prod.precio * prod.cantidad,0)
 }
+formularioCarrito.addEventListener('click', () => {
+    Swal.fire({
+        title: '¿Deseas finalizar tu compra? Esta accion sera irreversible',
+        showDenyButton: true,
+        confirmButtonText: 'Finalizar compra'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            botonesCarrito.innerHTML = ""
+    const div = document.createElement('div')
+    div.innerHTML = `<h1>INGRESE SUS DATOS</h1>
+    <div class="contact-card">
+        <div class="form">
+            <div class="column-1">
+                <label for="name">Nombre y apellido</label>
+                <label for="mail">Telefono</label>
+                <label for="mail">Direccion de correo</label>
+            </div>
+            <div class="column-2">
+                <input class="field" type="text" name="name" id="name">
+                <input class="field" type="text" name="phone" id="phone">
+                <input class="field" type="text" name="adress" id="adress">
+                <button onclick="mostrarMensajeAdress()" class="buton" id="enviarInfo">Enviar Info</button>
+            </div>
+        </div>
+    </div>`
+    formularioDiv.appendChild(div)
+        } else if (result.isDenied) {
+          Swal.fire('Siga tranquilimente con sus compras, gracias', '', 'info')
+        }
+      })
 
-/*localstorage*/
+})
+function mostrarMensajeAdress (){
+    let inputvalueName = document.getElementById("name").value;
+    let inputvalue = document.getElementById("adress").value;
+    let mensajeInputs = document.getElementById("mensaje");
+    datosDiv.innerHTML = ""
+    mensajeInputs.innerHTML =`Gracias ${inputvalueName} !!!, los datos fueron registrados correctamente, te mandaremos el resumen de su compra a esta direccion de correo electronico: ${inputvalue}`
+    carrito.length = 0
+    actualizarCarrito()
+}
 
